4e43c5bcf6609235f51ffe050e112ff3bd39ade4
dioo1461 <dioo1461@gmail.com>
2024-11-09 19:39:01 +0900

    feat(InputView.kt, OutputView.kt): 상품 선택 및 구매 기능 구현

--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -1,4 +1,20 @@
 package store.view
 
-class InputView {
+import camp.nextstep.edu.missionutils.Console
+
+
+object InputView {
+    fun requestPurchaseItemAndAmount(): String {
+        println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])")
+        return getInput()
+    }
+
+    fun requestYesOrNo(prompt: String): String {
+        println(prompt)
+        return getInput().trim().uppercase()
+    }
+
+    private fun getInput(): String {
+        return Console.readLine()
+    }
 }
--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -1,4 +1,40 @@
 package store.view
 
-class OutputView {
+import store.model.domain.Product
+import store.model.domain.Promotion
+import store.model.domain.PromotionType
+
+object OutputView {
+    fun printWelcomeMessage(
+        products: List<Product>,
+        promotions: List<Promotion>
+    ) {
+        println("안녕하세요. w편의점입니다.")
+        println("현재 보유하고 있는 상품입니다.")
+        printProductList(products, promotions)
+    }
+
+    private fun printProductList(products: List<Product>, promotions: List<Promotion>) {
+        products.forEach { product ->
+            val stockInfo = getStockInfo(product.stock)
+            val promotionInfo = getPromotionInfo(product.name, promotions)
+            println("${product.name} ${"%,d".format(product.price)}원 $stockInfo $promotionInfo")
+        }
+    }
+
+    private fun getStockInfo(stock: Int): String {
+        return if (stock > 0) "${stock}개" else "품절"
+    }
+
+    private fun getPromotionInfo(
+        productName: String,
+        promotions: List<Promotion>
+    ): String {
+        val promotion = promotions.find { it.productName == productName }
+        return when (promotion?.type) {
+            PromotionType.ONE_PLUS_ONE -> "${promotion.productName}1+1"
+            PromotionType.TWO_PLUS_ONE -> "${promotion.productName}2+1"
+            else -> ""
+        }
+    }
 }
