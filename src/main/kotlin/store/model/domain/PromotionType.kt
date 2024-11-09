e6d2425c35076af241ff3b4707c043ddbcb22363
dioo1461 <dioo1461@gmail.com>
2024-11-09 20:35:41 +0900

    feat(PromotionType.kt): None 필드 추가, PromotionType.fromString 메서드 추가

--- src/main/kotlin/store/model/domain/PromotionType.kt
+++ src/main/kotlin/store/model/domain/PromotionType.kt
@@ -1,6 +1,17 @@
 package store.model.domain
 
-enum class PromotionType(val n: Int) {
+enum class PromotionType(val additionalOfferingQuantity: Int) {
+    NONE(0),
     ONE_PLUS_ONE(1),
-    TWO_PLUS_ONE(2)
+    TWO_PLUS_ONE(2);
+
+    companion object {
+        fun fromString(type: String): PromotionType {
+            return when (type) {
+                "1+1" -> ONE_PLUS_ONE
+                "2+1" -> TWO_PLUS_ONE
+                else -> NONE
+            }
+        }
+    }
 }
